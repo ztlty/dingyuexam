8fcfe3eea976a30d75f8d66439b2131d08b02ff0
dingyu <0109@qq.com>
2019-01-10 01:52:14 +0000

    init blueking framework

---
+++ home_application/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns
+
+urlpatterns = patterns(
+    'home_application.views',
+    (r'^$', 'home'),
+    (r'^dev-guide/$', 'dev_guide'),
+    (r'^contactus/$', 'contactus'),
+)
